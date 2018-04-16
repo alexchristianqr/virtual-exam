b9913f7601ca19af93ea7e9156265c4a8531cc8d
Alex Christian <aquispe.developer@gmail.com>
2018-04-16 00:38:14 -0500

    creado service option answer

--- src/services/OptionAnswerService.js
+++ src/services/OptionAnswerService.js
@@ -1,1 +1,20 @@
+import Vue       from 'vue'
+import * as Vuex from 'vuex'
+import Axios     from 'axios'
+import Env       from '../env'
+
+Vue.use(Vuex)
+
+export default Vuex.Store({
+  actions: {
+    getOptionsAnswers({commit}, {self}) {
+      Axios.get(Env.API + '/get-options-anwers').then((r) => {
+        if(r.status === 200){
+          self.dataOptionAnswer = r.data
+        }
+      }).catch((e) => {})
+    },
+  },
+})
+
 
--- src/util.js
+++ src/util.js
@@ -48,4 +48,5 @@ export default {
   removeCookie(name,path) {
     document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT; path='+path;
   },
+
 }
