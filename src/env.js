ada1a94518b2f730c5b5563b454ed2024e61c38a
Alex Christian <aquispe.developer@gmail.com>
2018-05-22 13:25:02 -0500

    corregido error de url de restapi del backend

--- src/env.js
+++ src/env.js
@@ -1,4 +1,4 @@
 export default {
-  API_LARAVEL: 'https://restapis.sapia.com.pe:8077/api',//Api Database Master
+  API_LARAVEL: 'https://restapi.sapia.com.pe:8077/api',//Api Database Master
   API_NODEJS: 'https://restapi.sapia.com.pe:8090',//Api Authentication with Active Directory
 }
