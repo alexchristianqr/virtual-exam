0a760533005e2e277ae718c79fd3a7a3f5a21802
Alex Christian <aquispe.developer@gmail.com>
2018-06-16 22:43:29 -0500

    Modificado plugin sb-admin

--- src/assets/js/sb-admin.js
+++ src/assets/js/sb-admin.js
@@ -12,28 +12,35 @@ export default {
       $('[data-toggle="tooltip"]').tooltip({
         template: '<div class="tooltip navbar-sidenav-tooltip" role="tooltip" style="pointer-events: none;padding: 0 3.4rem;">' +
         '<div class="tooltip-inner"></div>' +
-        '</div>'
+        '</div>',
       })
 
       let scrollToTop = null
+
       // Toggle the side navigation
       $('#sidenavToggler').click((e) => {
-        e.preventDefault()
-        $('body').toggleClass('sidenav-toggled')
-        $('.navbar-sidenav .nav-link-collapse').addClass('collapsed')
-        $('.navbar-sidenav .sidenav-second-level, .navbar-sidenav .sidenav-third-level').removeClass('show')
+          e.preventDefault()
+          $('body').toggleClass('sidenav-toggled')
+          $('.navbar-sidenav .nav-link-collapse').addClass('collapsed')
+          $('.navbar-sidenav .sidenav-second-level, .navbar-sidenav .sidenav-third-level').removeClass('show')
       })
+
+
       // Force the toggled class to be removed when a collapsible nav link is clicked
+
       $('.navbar-sidenav .nav-link-collapse').click((e) => {
         e.preventDefault()
         $('body').removeClass('sidenav-toggled')
       })
+
       // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
+
       $('body.fixed-nav .navbar-sidenav, body.fixed-nav .sidenav-toggler, body.fixed-nav .navbar-collapse').on('mousewheel DOMMouseScroll', (e) => {
         let e0 = e.originalEvent, delta = e0.wheelDelta || -e0.detail
         this.scrollTop += (delta < 0 ? 1 : -1) * 30
         e.preventDefault()
       })
+
       // Scroll to top button appear
       $(document).scroll(() => {
         let scrollDistance = $(this).scrollTop()
@@ -43,6 +50,7 @@ export default {
           $('.scroll-to-top').fadeOut()
         }
       })
+
       // Smooth scrolling using jQuery easing
       $(document).on('click', 'a.scroll-to-top', (e) => {
         window.clearInterval(1)
@@ -56,7 +64,8 @@ export default {
         }, 16)
         e.preventDefault()
       })
+
       $('[data-toggle="popover"]').popover()
     }) // End of use strict
-  }
+  },
 }
